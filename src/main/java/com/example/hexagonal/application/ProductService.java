ea3ccf03261a6b7386529e91ac386073ee03088d
David Gil Galván <davidggalv@ext.inditex.com>
2024-07-09 10:27:04 +0200

    Curso clase 1

---
+++ src/main/java/com/example/hexagonal/application/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.hexagonal.application;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductService {
+
+  public String getProducts() {
+    // Implementación de obtención de productos
+    return "Lista de productos de España (Murcia)";
+  }
+}
---
+++ src/main/java/com/example/hexagonal/domain/Product.java
@@ -0,0 +1,39 @@
+package com.example.hexagonal.domain;
+
+
+public class Product {
+  private int id;
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  private String name;
+  private double price;
+
+  public Product(int id, String name, double price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+}
